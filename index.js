c034aa32bce1bf1222be8047524e348fca7a626e
torutilla <christiantorres0418@gmail.com>
2025-08-16 11:36:10 +0800

    added key_bindings

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Player from "./components/player.js";
-import Vector2 from "./components/vector.js";
+import Vector2 from "./components/type/vector.js";
 import SpriteOption from "./components/options/sprite_options.js";
 import AnimationPlayer from "./components/animation/animation_player.js";
 
@@ -21,8 +21,9 @@ const option = new SpriteOption({
     dWidth: 64, dHeight: 64, 
     totalFrames: 11, frameWidth: 32
 });
-var player = new Player({sprite_option: option});
 
+
+var player = new Player({sprite_option: option});
 var anim_player = new AnimationPlayer({sprite: player, ctx: ctx});
 requestAnimationFrame(anim_player.playAnimation);
     
