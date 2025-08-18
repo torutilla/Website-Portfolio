1b281b93680704b9946ac1b46a4310637c96a13a
torutilla <christiantorres0418@gmail.com>
2025-08-18 15:44:10 +0800

    added more methods on vector

--- components/math/vector.js
+++ components/math/vector.js
@@ -4,21 +4,40 @@ export default class Vector2{
         this.y = y;
     }
     add(vector){
+        return new Vector2(this.x + vector.x, this.y + vector.y);
+    }
+    addSelf(vector){
         this.x += vector.x;
         this.y += vector.y;
         return this;
     }
-    subtract(vector){
+    sub(vector){
+        return new Vector2(this.x - vector.x, this.y - vector.y);
+    }
+    subSelf(vector){
         this.x -= vector.x;
         this.y -= vector.y;
         return this;
     }
+    scale(s){
+        return new Vector2(this.x * s, this.y * s);
+    }
+    scaleSelf(s){
+        this.x *= s.x;
+        this.y *= s.y;
+        return this;
+    }
+    div(s){
+        return new Vector2(this.x /s, this.y/s);
+    }
+    divSelf(s){
+        this.x /= s.x;
+        this.y /= s.y;
+        return this;
+    }
     normalize(){
         const magnitude = this.magnitude();
-        if (magnitude === 0) return this;
-        this.x /= magnitude;
-        this.y /= magnitude;
-        return this;
+        return magnitude === 0? new Vector(0, 0) : this.div(magnitude);
     }
     magnitude(){
         return Math.hypot(this.x, this.y); 
@@ -28,5 +47,28 @@ export default class Vector2{
     }
     cross(vector){
         return this.x * vector.y - this.y * vector.x;
+    }
+    clone(){
+        return new Vector2(this.x, this.y);
+    }
+    angle(){
+        return Math.atan2(this.y, this.x);
+    }
+    distance(vector) {
+        return Math.hypot(this.x - v.x, this.y - v.y);
+    }
+    distanceSq(vector){
+        const dx = this.x - vector.x, dy = this.y - vector.y;
+        return dx*dx + dy*dy;
+    }
+    set(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    lerp(vector, factor){
+        return new Vector2(
+            this.x + (vector.x - this.x) * factor,
+            this.y + (vector.y - this.y) * factor,
+        );
     }
 }
