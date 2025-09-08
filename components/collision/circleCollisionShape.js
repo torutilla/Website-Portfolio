519ee733fa2e5d1cf471974f8afceede57c72ac2
torutilla <christiantorres0418@gmail.com>
2025-09-09 02:52:51 +0800

    added circle class and collision shape

--- components/collision/circleCollisionShape.js
+++ components/collision/circleCollisionShape.js
@@ -1,8 +1,21 @@
 import Circle from "../math/circle.js";
-
-export default class CircleCollisionShape{
+import Collision from "./collision.js";
+export default class CircleCollisionShape extends Collision{
     /**@param {Circle} circle  */
     constructor(circle){
         this.circle = circle;
+    }
+
+    /** @param {Collision} other  */
+    collidesWith(other){
+        if(other instanceof CircleCollisionShape) this.circle.intersectsCircle(other.circle)
+    }
+
+    debugDraw(ctx){
+        this.circle.draw(ctx);
+    }
+
+    getAABB(){
+        throw new Error('No bounding box in a circle');
     }
 }
--- components/math/circle.js
+++ components/math/circle.js
@@ -5,5 +5,34 @@ export default class Circle{
         this.center = new Vector2(x, y);
         this.radius = radius;
     }
+
+    
+
+    /**@param {Rect} rect  */
+    instersectsRect(rect){
+
+        const closestX = Math.max(rect.x, Math.min(this.center.x, rect.x + rect.width));
+        const closestY = Math.max(rect.y, Math.min(this.center.y, rect.y + rect.height));
+
+        const dx = this.center.x - closestX;
+        const dy = this.center.y - closestY;
+
+        return dx * dx + dy * dy <= this.radius * this.radius;
+    }
+
+    /**@param {Circle} other  */
+    intersectsCircle(other){
+        const dx = other.center.x - this.center.x;
+        const dy = other.center.y - this.center.y;
+        const rSum = this.radius + other.radius;
+
+        return dx * dx + dy * dy <= rSum * rSum; 
+    }
+    
+    draw(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2);
+        ctx.stroke();
+    }
     
 }
