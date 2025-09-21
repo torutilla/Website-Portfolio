30c7a3779a3580712b30a47af1f2a23570820c98
torutilla <christiantorres0418@gmail.com>
2025-09-22 03:15:29 +0800

    added methods to area2d

--- components/collision/area2d.js
+++ components/collision/area2d.js
@@ -1,15 +1,30 @@
-import Vector2 from "../math/vector.js";
 import Collision from "./collision.js";
 
 export default class Area2D {
     /**@param {Collision} collisionShape  */
     constructor(collisionShape){
+        this.id = crypto.randomUUID();
         this.collisionShape = collisionShape;
         this.collisionShape.collisionBlocking = false;
+        /**@type {Entity[]} */
+        this.bodies = [];
+        /**@type {Area2D[]} */
+        this.areas = [];
     }
 
     debugDraw(ctx){
         this.collisionShape.debugDraw(ctx);
     }
+
+    /**@param {Area2D} area  */
+    areaEntered(area){
+        if(!this.areas.some(a => a.id == area.id)){
+            this.areas.push(area);
+        }
+    }
+    areaExited(){}
+    /**@param {Entity} body  */
+    bodyEntered(body){}
+    bodyExited(){}
 
 }
