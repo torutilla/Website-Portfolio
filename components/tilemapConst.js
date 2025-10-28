6ed04d509560f827f8360a91369117b34cc7e570
torutilla <christiantorres0418@gmail.com>
2025-10-28 22:16:36 +0800

    change size of player and added bg trees

--- components/tilemapConst.js
+++ components/tilemapConst.js
@@ -7,11 +7,11 @@ export const terrainTilemap = {
     rect: new Rect(0, 0, 32, 32),
 };
 
-export const backgroundClouds = "../assets/Terrain/clouds.png";
+export const backgroundClouds = '../assets/Terrain/clouds.png';
 export const backgroundTrees = {
-    l1: "../assets/parallax-background/plx-1.png",
-    l2: "../assets/parallax-background/plx-2.png",
-    l3: "../assets/parallax-background/plx-3.png",
-    l4: "../assets/parallax-background/plx-4.png",
-    l5: "../assets/parallax-background/plx-5.png",
+    l1: '../assets/parallax background/plx-1.png',
+    l2: '../assets/parallax background/plx-2.png',
+    l3: '../assets/parallax background/plx-3.png',
+    l4: '../assets/parallax background/plx-4.png',
+    l5: '../assets/parallax background/plx-5.png',
 }
