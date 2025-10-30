8d7c55bc53a9db3602674f9beb88177ee4566495
torutilla <christiantorres0418@gmail.com>
2025-10-30 18:31:30 +0800

    changed tree bg png

--- components/tilemapConst.js
+++ components/tilemapConst.js
@@ -7,11 +7,9 @@ export const terrainTilemap = {
     rect: new Rect(0, 0, 32, 32),
 };
 
-export const backgroundClouds = '../assets/Terrain/clouds.png';
+export const backgroundClouds = '../assets/parallax background/Background.png';
 export const backgroundTrees = {
-    l1: '../assets/parallax background/plx-1.png',
-    l2: '../assets/parallax background/plx-2.png',
-    l3: '../assets/parallax background/plx-3.png',
-    l4: '../assets/parallax background/plx-4.png',
-    l5: '../assets/parallax background/plx-5.png',
+    l1: '../assets/parallax background/3.png',
+    l2: '../assets/parallax background/2.png',
+    l3: '../assets/parallax background/1.png',
 }
