9349579fabcce688d2c0eb95cc807b245737ec1a
torutilla <christiantorres0418@gmail.com>
2025-11-02 01:31:00 +0800

    added getCanvas method

--- components/systems/canvas/canvasHandler.js
+++ components/systems/canvas/canvasHandler.js
@@ -11,6 +11,15 @@ export default class CanvasHandler{
         return canvas;
     }
 
+    getCanvas(id, size = {x: window.innerWidth, y: window.innerHeight}){
+        const canvas = document.getElementById(id);
+        this.resizeCanvas(canvas, size);
+        const ctx = canvas.getContext('2d');
+        ctx.imageSmoothingEnabled = false;
+
+        return canvas;
+    }
+
     resizeCanvas(canvas, size){
         canvas.width = size.x;
         canvas.height = size.y;
