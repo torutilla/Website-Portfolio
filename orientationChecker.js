cd1210e02b27ea7fc98e447344ef7d1c17aebbe1
torutilla <christiantorres0418@gmail.com>
2025-11-15 15:28:49 +0800

    hide orientationblocker

--- orientationChecker.js
+++ orientationChecker.js
@@ -4,11 +4,11 @@ import UserInterfaceController from "./components/systems/user_interface/uiContr
 const uiController = new UserInterfaceController('main-ui');
 export default function checkOrientation(){
     world.resizeWorld();
-    if(window.innerHeight > window.innerWidth){
-        document.getElementById('orientation-blocker').style.display = 'flex';
-    }else{
-        document.getElementById('orientation-blocker').style.display = 'none';
-    }
+    // if(window.innerHeight > window.innerWidth){
+    //     document.getElementById('orientation-blocker').style.display = 'flex';
+    // }else{
+    //     document.getElementById('orientation-blocker').style.display = 'none';
+    // }
     if(isDesktop()){
         uiController.hideMobileHud('mobile-hud');
     }else{
