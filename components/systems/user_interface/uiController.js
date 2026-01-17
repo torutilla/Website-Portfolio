567323ce093e552a067fbd0a3ada380750eeb9af
torutilla <christiantorres0418@gmail.com>
2026-01-17 20:15:42 +0800

    paused inputs on interaction

--- components/systems/user_interface/uiController.js
+++ components/systems/user_interface/uiController.js
@@ -2,7 +2,7 @@ import EventBus from "../event/eventBus.js";
 import  AreaUIController from "./areaUIController.js";
 import InteractionUI from "./interactionUI.js";
 import MobileHUD from "./mobileHUD.js";
-
+import InputManager from "../key_bindings/Input.js";
 export default class UserInterfaceController extends EventBus{
     constructor(id){
         super(id);
@@ -25,17 +25,27 @@ export default class UserInterfaceController extends EventBus{
     /**@param {()=> void} onInteract */
     add_interaction_button(entity, onInteract, btnText = "INTERACT"){
         this.interactionController.addOption(entity, btnText);
-        this.interactionController.on('interact', onInteract);
+        entity._onInteract = onInteract;
+        this.interactionController.on('interact', entity._onInteract);
     }
 
     remove_interaction_button(entity){
         this.interactionController.removeOption(entity);
+        if(entity._onInteract){
+            this.interactionController.off('interact', entity._onInteract);
+            delete entity._onInteract;
+        }
     }
 
-    showAreaUI(type){
+    showAreaUI(type, entity){
+        this.mobileHud.hide();
+        this.interactionController.off('interact', entity._onInteract)
+        InputManager.pause_input = true;
         this.areaController.displayArea(type);
         const handleClose = () =>{
-            console.log("area_closed");
+            this.mobileHud.display();
+            InputManager.pause_input = false;
+            this.interactionController.on('interact', entity._onInteract);
         }
         this.areaController.on('on_ui_close', handleClose)
     }
