bf0f7472d8f1dddc028f93114bf8c2c526da9aa9
torutilla <christiantorres0418@gmail.com>
2026-01-26 22:45:37 +0800

    update projects appearance

--- components/ui/areas/projectsSection.js
+++ components/ui/areas/projectsSection.js
@@ -2,6 +2,48 @@ import SpriteImage from "../../options/sprite_options.js";
 import Vector2 from "../../math/vector.js"
 import AnimationPlayer from "../../animation/animation_player.js";
 import Sprite from "../../type/sprite.js";
+
+/**@typedef {{title: string, stack: string[], description?: string}} Info */
+
+/** @type {{ [key: string]: Info[] }} */
+const projects = {
+    game: [
+        {
+            title: "Bow & Arrow Recreation",
+            stack: ["Godot Engine"]
+        },
+        {
+            title: "Interactive Resume",
+            stack: ["JavaScript", "HTML", "CSS"]
+        },
+        {
+            title: "Action RPG",
+            stack: ["Godot Engine"]
+        },
+    ],
+    mobile: [
+        {
+            title: "Mobile Booking / Taxi App for Trike Vehicle",
+            stack: ["Flutter", "Dart", "Firebase", "Google Maps API"]
+        },
+    ],
+    web: [
+        {
+            title: "Interactive Resume",
+            stack: ["JavaScript", "HTML", "CSS"]
+        },
+        {
+            title: "Web Portfolio",
+            stack: ["TypeScript", "React", "Tailwind CSS"]
+        },
+    ],
+    graphic:[
+        {
+            title: "",
+            stack: []
+        },
+    ]
+}
 export default function Projects(){
     const desk = document.createElement('div');
     desk.classList.add('desk-ui');
@@ -12,10 +54,12 @@ export default function Projects(){
     canvasContainer.classList.add('desk-content');
     
     const contentContainer = document.createElement('div');
-    const title = document.createElement('p');
-    title.innerText = "Projects";
-    contentContainer.appendChild(title);
+    contentContainer.classList.add('desk-content-container');
+    
+    let section = SectionCreator({title: "Game Development", content: projects.game});
 
+    contentContainer.append(...section);
+    
     const baseSize = new Vector2(224, 224);
     canvas.width = baseSize.x
     canvas.height = baseSize.y
@@ -51,4 +95,49 @@ export default function Projects(){
     desk.append(canvasContainer);
     
     return desk;
+}
+
+/**@param {{title: string, content: Info[]}}  */
+function SectionCreator({title, content}){
+    /**@type {HTMLElement[]} */
+    let children = [];
+    const holder = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = "/assets/ui/header.png";
+    holder.classList.add('desk-title-container');
+    const h1 = document.createElement('h1');
+    h1.innerText = "Projects";
+    const p = document.createElement('p');
+    p.innerText = title;
+    holder.append(img, h1, p)
+
+    const contentDiv = document.createElement('div');
+    contentDiv.classList.add('project-content-container');
+    
+    for(let i of content){
+        const projectContent = document.createElement('div');
+        projectContent.classList.add('project-content');
+        
+        const projectTitle = document.createElement('h3');
+        projectTitle.innerText = i.title;
+
+        const stackContainer = document.createElement('div');
+        stackContainer.classList.add('tech-stack-container');
+        for(let stack of i.stack){
+            const p = document.createElement('p');
+            p.innerText = stack;
+            stackContainer.appendChild(p);
+        }
+        
+        projectContent.appendChild(projectTitle)
+        if(i.description){
+            const projectDescription = document.createElement('p');
+            projectDescription.innerText = i.description;
+            projectContent.appendChild(projectDescription);
+        }
+        projectContent.appendChild(stackContainer)
+        contentDiv.appendChild(projectContent);
+    }
+    children.push(holder, contentDiv);
+    return children;
 }
